5ed4b1cbcc87993afa1e461d34bc1917e05222ef
DhruvTemura <dhruvtemura@gmail.com>
2025-04-19 04:13:32 +0530

    login 1

---
+++ frontend/src/Pages/Login.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+  const [error, setError] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
